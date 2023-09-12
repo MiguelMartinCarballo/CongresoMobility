59f31ad367dd3eab2747c4c0de11c908611bba1d
MiguelMartinCarballo <miguelmc.es@gmail.com>
2023-09-12 11:30:01 +0200

    .

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("logo_principal").style.height = "40px";
+    document.getElementById("logo_principal").style.height += 20;
     document.getElementById("btn_menu").style.padding = "0px 12px";
     document.getElementById("header").classList.add("shadow");
   } else {
-    document.getElementById("logo_principal").style.height = "60px";
+    var pixels = document.getElementById("logo_principal").style.height - 20;
+    var pixelss = pixels + "px";
+    document.getElementById("logo_principal").style.height = pixelss;
     document.getElementById("btn_menu").style.padding = "9px 12px";
     document.getElementById("header").classList.remove("shadow");
   }
